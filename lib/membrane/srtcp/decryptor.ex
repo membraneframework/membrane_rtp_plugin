d5171543d40452ab12e758b388f2f70831693054
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-03-14 11:58:50 +0100

    Fix compiation withou ex_libsrtp

--- lib/membrane/srtcp/decryptor.ex
+++ lib/membrane/srtcp/decryptor.ex
@@ -1,69 +1,71 @@
-defmodule Membrane.SRTCP.Decryptor do
-  @moduledoc """
-  Converts SRTCP packets to plain RTCP.
+if Code.ensure_loaded?(ExLibSRTP) do
+  defmodule Membrane.SRTCP.Decryptor do
+    @moduledoc """
+    Converts SRTCP packets to plain RTCP.
 
-  Requires adding [libsrtp](https://github.com/membraneframework/elixir_libsrtp) dependency to work.
-  """
-  use Membrane.Filter
+    Requires adding [libsrtp](https://github.com/membraneframework/elixir_libsrtp) dependency to work.
+    """
+    use Membrane.Filter
 
-  alias Membrane.Buffer
+    alias Membrane.Buffer
 
-  def_input_pad :input, caps: :any, demand_mode: :auto
-  def_output_pad :output, caps: :any, demand_mode: :auto
+    def_input_pad :input, caps: :any, demand_mode: :auto
+    def_output_pad :output, caps: :any, demand_mode: :auto
 
-  def_options policies: [
-                spec: [ExLibSRTP.Policy.t()],
-                description: """
-                List of SRTP policies to use for decrypting packets.
-                See `t:ExLibSRTP.Policy.t/0` for details.
-                """
-              ]
+    def_options policies: [
+                  spec: [ExLibSRTP.Policy.t()],
+                  description: """
+                  List of SRTP policies to use for decrypting packets.
+                  See `t:ExLibSRTP.Policy.t/0` for details.
+                  """
+                ]
 
-  @impl true
-  def handle_init(options) do
-    {:ok, Map.from_struct(options) |> Map.merge(%{srtp: nil})}
+    @impl true
+    def handle_init(options) do
+      {:ok, Map.from_struct(options) |> Map.merge(%{srtp: nil})}
+    end
+
+    @impl true
+    def handle_stopped_to_prepared(_ctx, state) do
+      srtp = ExLibSRTP.new()
+
+      state.policies
+      |> Bunch.listify()
+      |> Enum.each(&ExLibSRTP.add_stream(srtp, &1))
+
+      {:ok, %{state | srtp: srtp}}
+    end
+
+    @impl true
+    def handle_prepared_to_stopped(_ctx, state) do
+      {:ok, %{state | srtp: nil}}
+    end
+
+    @impl true
+    def handle_process(:input, buffer, _ctx, state) do
+      {:ok, payload} = ExLibSRTP.unprotect_rtcp(state.srtp, buffer.payload)
+      {{:ok, buffer: {:output, %Buffer{buffer | payload: payload}}}, state}
+    end
+
+    @impl true
+    def handle_event(_pad, %{handshake_data: handshake_data}, _ctx, %{policies: []} = state) do
+      {_local_keying_material, remote_keying_material, protection_profile} = handshake_data
+
+      {:ok, crypto_profile} =
+        ExLibSRTP.Policy.crypto_profile_from_dtls_srtp_protection_profile(protection_profile)
+
+      policy = %ExLibSRTP.Policy{
+        ssrc: :any_inbound,
+        key: remote_keying_material,
+        rtp: crypto_profile,
+        rtcp: crypto_profile
+      }
+
+      :ok = ExLibSRTP.add_stream(state.srtp, policy)
+      {:ok, %{state | policies: [policy]}}
+    end
+
+    @impl true
+    def handle_event(pad, event, ctx, state), do: super(pad, event, ctx, state)
   end
-
-  @impl true
-  def handle_stopped_to_prepared(_ctx, state) do
-    srtp = ExLibSRTP.new()
-
-    state.policies
-    |> Bunch.listify()
-    |> Enum.each(&ExLibSRTP.add_stream(srtp, &1))
-
-    {:ok, %{state | srtp: srtp}}
-  end
-
-  @impl true
-  def handle_prepared_to_stopped(_ctx, state) do
-    {:ok, %{state | srtp: nil}}
-  end
-
-  @impl true
-  def handle_process(:input, buffer, _ctx, state) do
-    {:ok, payload} = ExLibSRTP.unprotect_rtcp(state.srtp, buffer.payload)
-    {{:ok, buffer: {:output, %Buffer{buffer | payload: payload}}}, state}
-  end
-
-  @impl true
-  def handle_event(_pad, %{handshake_data: handshake_data}, _ctx, %{policies: []} = state) do
-    {_local_keying_material, remote_keying_material, protection_profile} = handshake_data
-
-    {:ok, crypto_profile} =
-      ExLibSRTP.Policy.crypto_profile_from_dtls_srtp_protection_profile(protection_profile)
-
-    policy = %ExLibSRTP.Policy{
-      ssrc: :any_inbound,
-      key: remote_keying_material,
-      rtp: crypto_profile,
-      rtcp: crypto_profile
-    }
-
-    :ok = ExLibSRTP.add_stream(state.srtp, policy)
-    {:ok, %{state | policies: [policy]}}
-  end
-
-  @impl true
-  def handle_event(pad, event, ctx, state), do: super(pad, event, ctx, state)
 end
--- lib/membrane/srtp/encryptor.ex
+++ lib/membrane/srtp/encryptor.ex
@@ -1,128 +1,130 @@
-defmodule Membrane.SRTP.Encryptor do
-  @moduledoc """
-  Converts plain RTP packets to SRTP.
+if Code.ensure_loaded?(ExLibSRTP) do
+  defmodule Membrane.SRTP.Encryptor do
+    @moduledoc """
+    Converts plain RTP packets to SRTP.
 
-  Requires adding [srtp](https://github.com/membraneframework/elixir_libsrtp) dependency to work.
-  """
-  use Membrane.Filter
+    Requires adding [srtp](https://github.com/membraneframework/elixir_libsrtp) dependency to work.
+    """
+    use Membrane.Filter
 
-  alias Membrane.{Buffer, RTP}
+    alias Membrane.{Buffer, RTP}
 
-  require Membrane.Logger
+    require Membrane.Logger
 
-  def_input_pad :input, caps: :any, demand_mode: :auto
-  def_output_pad :output, caps: :any, demand_mode: :auto
+    def_input_pad :input, caps: :any, demand_mode: :auto
+    def_output_pad :output, caps: :any, demand_mode: :auto
 
-  def_options policies: [
-                spec: [ExLibSRTP.Policy.t()],
-                default: [],
-                description: """
-                List of SRTP policies to use for encrypting packets.
-                See `t:ExLibSRTP.Policy.t/0` for details.
-                """
-              ]
+    def_options policies: [
+                  spec: [ExLibSRTP.Policy.t()],
+                  default: [],
+                  description: """
+                  List of SRTP policies to use for encrypting packets.
+                  See `t:ExLibSRTP.Policy.t/0` for details.
+                  """
+                ]
 
-  @impl true
-  def handle_init(%__MODULE__{policies: policies}) do
-    state = %{
-      policies: policies,
-      srtp: nil,
-      queue: []
-    }
+    @impl true
+    def handle_init(%__MODULE__{policies: policies}) do
+      state = %{
+        policies: policies,
+        srtp: nil,
+        queue: []
+      }
 
-    {:ok, state}
-  end
-
-  @impl true
-  def handle_stopped_to_prepared(_ctx, state) do
-    srtp = ExLibSRTP.new()
-
-    state.policies
-    |> Bunch.listify()
-    |> Enum.each(&ExLibSRTP.add_stream(srtp, &1))
-
-    {:ok, %{state | srtp: srtp}}
-  end
-
-  @impl true
-  def handle_start_of_stream(:input, _ctx, state) do
-    if state.policies == [] do
-      # TODO: remove when dynamic switching between automatic and manual demands will be supported
-      {{:ok, start_timer: {:policy_timer, Membrane.Time.seconds(5)}}, state}
-    else
       {:ok, state}
     end
-  end
 
-  @impl true
-  def handle_tick(:policy_timer, ctx, state) do
-    if state.policies != [] or ctx.pads.input.end_of_stream? do
-      {{:ok, stop_timer: :policy_timer}, state}
-    else
-      raise "No SRTP policies arrived in 5 seconds"
+    @impl true
+    def handle_stopped_to_prepared(_ctx, state) do
+      srtp = ExLibSRTP.new()
+
+      state.policies
+      |> Bunch.listify()
+      |> Enum.each(&ExLibSRTP.add_stream(srtp, &1))
+
+      {:ok, %{state | srtp: srtp}}
     end
-  end
 
-  @impl true
-  def handle_prepared_to_stopped(_ctx, state) do
-    {:ok, %{state | srtp: nil, policies: []}}
-  end
-
-  @impl true
-  def handle_event(_pad, %{handshake_data: handshake_data}, _ctx, %{policies: []} = state) do
-    {local_keying_material, _remote_keying_material, protection_profile} = handshake_data
-
-    {:ok, crypto_profile} =
-      ExLibSRTP.Policy.crypto_profile_from_dtls_srtp_protection_profile(protection_profile)
-
-    policy = %ExLibSRTP.Policy{
-      ssrc: :any_outbound,
-      key: local_keying_material,
-      rtp: crypto_profile,
-      rtcp: crypto_profile
-    }
-
-    :ok = ExLibSRTP.add_stream(state.srtp, policy)
-    buffers = state.queue |> Enum.reverse() |> Enum.flat_map(&protect_buffer(&1, state.srtp))
-    {{:ok, buffer: {:output, buffers}}, %{Map.put(state, :policies, [policy]) | queue: []}}
-  end
-
-  @impl true
-  def handle_event(_pad, other, _ctx, state) do
-    Membrane.Logger.warn("Got unexpected event: #{inspect(other)}. Ignoring.")
-    {:ok, state}
-  end
-
-  @impl true
-  def handle_process(:input, buffer, _ctx, %{policies: []} = state) do
-    {:ok, Map.update!(state, :queue, &[buffer | &1])}
-  end
-
-  @impl true
-  def handle_process(:input, buffer, _ctx, state) do
-    {{:ok, buffer: {:output, protect_buffer(buffer, state.srtp)}}, state}
-  end
-
-  defp protect_buffer(buffer, srtp) do
-    %Buffer{payload: payload} = buffer
-    packet_type = RTP.Packet.identify(payload)
-
-    case packet_type do
-      :rtp -> ExLibSRTP.protect(srtp, payload)
-      :rtcp -> ExLibSRTP.protect_rtcp(srtp, payload)
+    @impl true
+    def handle_start_of_stream(:input, _ctx, state) do
+      if state.policies == [] do
+        # TODO: remove when dynamic switching between automatic and manual demands will be supported
+        {{:ok, start_timer: {:policy_timer, Membrane.Time.seconds(5)}}, state}
+      else
+        {:ok, state}
+      end
     end
-    |> case do
-      {:ok, payload} ->
-        [%Buffer{buffer | payload: payload}]
 
-      {:error, reason} ->
-        Membrane.Logger.warn("""
-        Couldn't protect #{packet_type} payload:
-        #{inspect(payload, limit: :infinity)}
-        Reason: #{inspect(reason)}. Ignoring packet.
-        """)
+    @impl true
+    def handle_tick(:policy_timer, ctx, state) do
+      if state.policies != [] or ctx.pads.input.end_of_stream? do
+        {{:ok, stop_timer: :policy_timer}, state}
+      else
+        raise "No SRTP policies arrived in 5 seconds"
+      end
+    end
 
-        []
+    @impl true
+    def handle_prepared_to_stopped(_ctx, state) do
+      {:ok, %{state | srtp: nil, policies: []}}
+    end
+
+    @impl true
+    def handle_event(_pad, %{handshake_data: handshake_data}, _ctx, %{policies: []} = state) do
+      {local_keying_material, _remote_keying_material, protection_profile} = handshake_data
+
+      {:ok, crypto_profile} =
+        ExLibSRTP.Policy.crypto_profile_from_dtls_srtp_protection_profile(protection_profile)
+
+      policy = %ExLibSRTP.Policy{
+        ssrc: :any_outbound,
+        key: local_keying_material,
+        rtp: crypto_profile,
+        rtcp: crypto_profile
+      }
+
+      :ok = ExLibSRTP.add_stream(state.srtp, policy)
+      buffers = state.queue |> Enum.reverse() |> Enum.flat_map(&protect_buffer(&1, state.srtp))
+      {{:ok, buffer: {:output, buffers}}, %{Map.put(state, :policies, [policy]) | queue: []}}
+    end
+
+    @impl true
+    def handle_event(_pad, other, _ctx, state) do
+      Membrane.Logger.warn("Got unexpected event: #{inspect(other)}. Ignoring.")
+      {:ok, state}
+    end
+
+    @impl true
+    def handle_process(:input, buffer, _ctx, %{policies: []} = state) do
+      {:ok, Map.update!(state, :queue, &[buffer | &1])}
+    end
+
+    @impl true
+    def handle_process(:input, buffer, _ctx, state) do
+      {{:ok, buffer: {:output, protect_buffer(buffer, state.srtp)}}, state}
+    end
+
+    defp protect_buffer(buffer, srtp) do
+      %Buffer{payload: payload} = buffer
+      packet_type = RTP.Packet.identify(payload)
+
+      case packet_type do
+        :rtp -> ExLibSRTP.protect(srtp, payload)
+        :rtcp -> ExLibSRTP.protect_rtcp(srtp, payload)
+      end
+      |> case do
+        {:ok, payload} ->
+          [%Buffer{buffer | payload: payload}]
+
+        {:error, reason} ->
+          Membrane.Logger.warn("""
+          Couldn't protect #{packet_type} payload:
+          #{inspect(payload, limit: :infinity)}
+          Reason: #{inspect(reason)}. Ignoring packet.
+          """)
+
+          []
+      end
     end
   end
 end
