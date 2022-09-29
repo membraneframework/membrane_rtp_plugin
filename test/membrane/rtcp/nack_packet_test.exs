1afa97c90e3e35b392c733a85caad5c658033298
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2022-09-29 11:47:05 +0200

    Improve readability in tests

--- test/membrane/rtcp/nack_packet_test.exs
+++ test/membrane/rtcp/nack_packet_test.exs
@@ -5,17 +5,19 @@ defmodule Membrane.RTCP.TransportFeedbackPacket.NACKTest do
 
   @examples %{
     # BLP == 0, only sequence number passed via PID lost
-    "pid_only" => {<<2137::16, 0::16>>, [2137]},
+    "one ID lost" => {<<2137::16, 0::16>>, [2137]},
     # All bits of BLP set to 1, lost packets from PID to PID+16
-    "all lost" => {<<2137::16, 0xFFFF::16>>, 2137..(2137 + 16)//1},
+    "17 consecutive IDs lost" => {<<2137::16, 0xFFFF::16>>, 2137..(2137 + 16)//1},
     # Two least significant bits of BLP set to 1, lost packets from PID to PID+2
-    "3 consecutive lost" => {<<2137::16, 0::14, 0b11::2>>, 2137..(2137 + 2)//1},
+    "3 consecutive IDs lost" => {<<2137::16, 0::14, 0b11::2>>, 2137..(2137 + 2)//1},
     # PID, PID + 2 and PID + 4 missing
-    "3 with step 2 lost" => {<<2137::16, 0::12, 1::1, 0::1, 1::1, 0::1>>, [2137, 2139, 2141]},
-    # Two FCI combined to report 18 packets lost
-    "18 consecutive" => {<<2137::16, 0xFFFF::16, 2137 + 17::16, 0::16>>, 2137..(2137 + 17)//1}
+    "3 IDs with step 2 lost" => {<<2137::16, 0::12, 1::1, 0::1, 1::1, 0::1>>, [2137, 2139, 2141]},
+    # Two FCIs combined to report 18 packets lost
+    "18 consecutive IDs lost" =>
+      {<<2137::16, 0xFFFF::16, 2137 + 17::16, 0::16>>, 2137..(2137 + 17)//1}
   }
-  describe "decoding NACKs FCI" do
+
+  describe "NACK.decode/1 decodes" do
     for name <- Map.keys(@examples) do
       test name do
         {binary, expected_ids} = Map.fetch!(@examples, unquote(name))
@@ -25,7 +27,7 @@ defmodule Membrane.RTCP.TransportFeedbackPacket.NACKTest do
     end
   end
 
-  describe "encoding NACKs FCI" do
+  describe "NACK.encode/1 encodes" do
     for name <- Map.keys(@examples) do
       test name do
         {expected_binary, ids} = Map.fetch!(@examples, unquote(name))
