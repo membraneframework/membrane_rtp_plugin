f3ae86364cda1f4772e296d94343a8c739a09c52
Michał Śledź <michalsledz34@gmail.com>
2023-02-07 13:35:36 +0100

    Replace buffer store with a map

--- lib/membrane/rtp/outbound_rtx_controller.ex
+++ lib/membrane/rtp/outbound_rtx_controller.ex
@@ -3,7 +3,6 @@ defmodule Membrane.RTP.OutboundRtxController do
 
   require Membrane.Logger
 
-  alias Membrane.RTP.JitterBuffer.BufferStore
   alias Membrane.RTP.RetransmissionRequest
 
   def_input_pad :input,
@@ -20,23 +19,15 @@ defmodule Membrane.RTP.OutboundRtxController do
   @min_rtx_interval 10
 
   @impl true
-  def handle_init(_opts), do: {:ok, %{store: BufferStore.new(), last_rtx_times: %{}}}
+  def handle_init(_opts), do: {:ok, %{store: %{}, last_rtx_times: %{}}}
 
   # Ignores padding packets
   # TODO: Should it?
   @impl true
   def handle_process(:input, buffer, _ctx, state) when byte_size(buffer.payload) > 0 do
-    state
-    |> Map.update!(:store, fn store ->
-      case BufferStore.insert_buffer(store, buffer) do
-        {:ok, new_store} ->
-          maintain_store_size(new_store)
-
-        {:error, :late_packet} ->
-          store
-      end
-    end)
-    |> then(&{{:ok, forward: buffer}, &1})
+    idx = rem(buffer.metadata.rtp.sequence_number, @max_store_size)
+    state = put_in(state, [:store, idx], {System.monotonic_time(:millisecond), buffer})
+    {{:ok, forward: buffer}, state}
   end
 
   @impl true
@@ -51,49 +42,35 @@ defmodule Membrane.RTP.OutboundRtxController do
       ) do
     now = System.monotonic_time(:millisecond)
 
-    buffers_to_retransmit =
-      sequence_numbers
-      |> Stream.map(fn seq_num -> BufferStore.get_buffer(state.store, seq_num) end)
-      |> Stream.filter(fn
-        {:ok, buffer} ->
-          seq_num = buffer.metadata.rtp.sequence_number
-          last_rtx_time = Map.get(state.last_rtx_times, seq_num, now - @min_rtx_interval)
-
-          now - last_rtx_time >= @min_rtx_interval
-
-        {:error, :not_found} ->
-          false
+    {buffers, store} =
+      Enum.map_reduce(sequence_numbers, state.store, fn seq_num, store ->
+        maybe_retransmit(seq_num, now, store)
       end)
-      |> Enum.map(fn {:ok, buffer} -> buffer end)
 
-    state =
-      Map.update!(state, :last_rtx_times, fn times ->
-        updates =
-          Map.new(buffers_to_retransmit, fn buffer ->
-            {buffer.metadata.rtp.sequence_number, now}
-          end)
-
-        Map.merge(times, updates)
-      end)
+    buffers_to_retransmit = Enum.reject(buffers, fn buffer -> buffer == nil end)
 
     unless buffers_to_retransmit == [],
       do:
         Membrane.Logger.info(
-          "Retransmitting buffers with the following sequence numbers:\n#{inspect(Enum.map(buffers_to_retransmit, & &1.metadata.rtp.sequence_number))}"
+          "Retransmitting buffers (#{Enum.count(buffers_to_retransmit)}) with the following sequence numbers:\n#{inspect(Enum.map(buffers_to_retransmit, & &1.metadata.rtp.sequence_number))}"
         )
 
-    {{:ok, buffer: {:output, buffers_to_retransmit}}, state}
+    {{:ok, buffer: {:output, buffers_to_retransmit}}, %{state | store: store}}
   end
 
   @impl true
   def handle_event(pad, event, ctx, state), do: super(pad, event, ctx, state)
 
-  defp maintain_store_size(store) do
-    if Enum.count(store) > @max_store_size do
-      {_entry, store} = BufferStore.flush_one(store)
-      maintain_store_size(store)
+  defp maybe_retransmit(seq_num, now, store) do
+    idx = rem(seq_num, @max_store_size)
+    {last_rtx_timestamp, buffer} = Map.get(store, idx, {nil, nil})
+
+    if buffer != nil and buffer.metadata.rtp.sequence_number == seq_num and
+         now - last_rtx_timestamp >= @min_rtx_interval do
+      store = Map.put(store, seq_num, {now, buffer})
+      {buffer, store}
     else
-      store
+      {nil, store}
     end
   end
 end
