b289e24513156389527f0186b9c6e5ee3f05a459
Michał Śledź <michalsledz34@gmail.com>
2023-02-10 09:50:21 +0100

    Don't ignore first NACK if it comes before min_rtx_interval

--- lib/membrane/rtp/outbound_rtx_controller.ex
+++ lib/membrane/rtp/outbound_rtx_controller.ex
@@ -26,7 +26,7 @@ defmodule Membrane.RTP.OutboundRtxController do
   @impl true
   def handle_process(:input, buffer, _ctx, state) when byte_size(buffer.payload) > 0 do
     idx = rem(buffer.metadata.rtp.sequence_number, @max_store_size)
-    state = put_in(state, [:store, idx], {System.monotonic_time(:millisecond), buffer})
+    state = put_in(state, [:store, idx], {nil, buffer})
     {{:ok, forward: buffer}, state}
   end
 
@@ -47,13 +47,14 @@ defmodule Membrane.RTP.OutboundRtxController do
         maybe_retransmit(seq_num, now, store)
       end)
 
-    buffers_to_retransmit = Enum.reject(buffers, fn buffer -> buffer == nil end)
+    buffers_to_retransmit = Enum.reject(buffers, &is_nil/1)
 
-    unless buffers_to_retransmit == [],
-      do:
-        Membrane.Logger.info(
-          "Retransmitting buffers (#{Enum.count(buffers_to_retransmit)}) with the following sequence numbers:\n#{inspect(Enum.map(buffers_to_retransmit, & &1.metadata.rtp.sequence_number))}"
-        )
+    unless buffers_to_retransmit == [] do
+      Membrane.Logger.info("""
+      Retransmitting (#{length(buffers_to_retransmit)}) buffers, sequence numbers:
+      #{inspect(Enum.map(buffers_to_retransmit, & &1.metadata.rtp.sequence_number))}
+      """)
+    end
 
     {{:ok, buffer: {:output, buffers_to_retransmit}}, %{state | store: store}}
   end
@@ -63,14 +64,17 @@ defmodule Membrane.RTP.OutboundRtxController do
 
   defp maybe_retransmit(seq_num, now, store) do
     idx = rem(seq_num, @max_store_size)
-    {last_rtx_timestamp, buffer} = Map.get(store, idx, {nil, nil})
+    {last_rtx_time, buffer} = Map.get(store, idx, {nil, nil})
 
     if buffer != nil and buffer.metadata.rtp.sequence_number == seq_num and
-         now - last_rtx_timestamp >= @min_rtx_interval do
-      store = Map.put(store, seq_num, {now, buffer})
+         time_elapsed?(last_rtx_time, now) do
+      store = Map.put(store, idx, {now, buffer})
       {buffer, store}
     else
       {nil, store}
     end
   end
+
+  defp time_elapsed?(nil, _now), do: true
+  defp time_elapsed?(last_rtx_time, now), do: now - last_rtx_time >= @min_rtx_interval
 end
