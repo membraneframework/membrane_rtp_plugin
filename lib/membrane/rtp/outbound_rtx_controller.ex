b4a9b120e4a607460d0691cafbf5bc9da125adb7
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2023-02-20 14:55:49 +0100

    Migrate Out RTX Contoller to use map

--- lib/membrane/rtp/outbound_rtx_controller.ex
+++ lib/membrane/rtp/outbound_rtx_controller.ex
@@ -3,7 +3,6 @@ defmodule Membrane.RTP.OutboundRtxController do
 
   require Membrane.Logger
 
-  alias Membrane.RTP.JitterBuffer.BufferStore
   alias Membrane.RTP.RetransmissionRequestEvent
 
   def_input_pad :input,
@@ -19,22 +18,20 @@ defmodule Membrane.RTP.OutboundRtxController do
   @max_store_size 300
   @min_rtx_interval 10
 
+  @doc false
+  @spec max_store_size() :: pos_integer()
+  def max_store_size(), do: @max_store_size
+
   @impl true
-  def handle_init(_ctx, _opts), do: {[], %{store: %BufferStore{}, last_rtx_times: %{}}}
+  def handle_init(_ctx, _opts), do: {[], %{store: %{}}}
 
   @impl true
   def handle_process(:input, buffer, _ctx, state) when byte_size(buffer.payload) > 0 do
-    state
-    |> Map.update!(:store, fn store ->
-      case BufferStore.insert_buffer(store, buffer) do
-        {:ok, new_store} ->
-          maintain_store_size(new_store)
+    idx = sn_to_index(buffer.metadata.rtp.sequence_number)
 
-        {:error, :late_packet} ->
-          store
-      end
-    end)
-    |> then(&{[forward: buffer], &1})
+    state = put_in(state, [:store, idx], {nil, buffer})
+
+    {[forward: buffer], state}
   end
 
   @impl true
@@ -47,49 +44,46 @@ defmodule Membrane.RTP.OutboundRtxController do
         _ctx,
         state
       ) do
-    Membrane.Logger.info("Got RTX request #{inspect(sequence_numbers)}")
+    Membrane.Logger.debug(
+      "Got RTX request of size #{length(sequence_numbers)}: #{inspect(sequence_numbers)}"
+    )
+
     now = System.monotonic_time(:millisecond)
 
-    buffers_to_retransmit =
-      sequence_numbers
-      |> Stream.map(fn seq_num -> BufferStore.get_buffer(state.store, seq_num) end)
-      |> Stream.filter(fn
-        {:ok, buffer} ->
-          seq_num = buffer.metadata.rtp.sequence_number
-          last_rtx_time = Map.get(state.last_rtx_times, seq_num, now - @min_rtx_interval)
-
-          now - last_rtx_time >= @min_rtx_interval
-
-        {:error, :not_found} ->
-          false
+    {buffers, store} =
+      Enum.map_reduce(sequence_numbers, state.store, fn seq_num, store ->
+        maybe_retransmit(seq_num, now, store)
       end)
-      |> Enum.map(fn {:ok, buffer} -> buffer end)
 
-    seq_nums_to_retransmit = Enum.map(buffers_to_retransmit, & &1.metadata.rtp.sequence_number)
-
-    state =
-      Map.update!(state, :last_rtx_times, fn times ->
-        updates = Map.from_keys(seq_nums_to_retransmit, now)
-
-        Map.merge(times, updates)
-      end)
+    buffers_to_retransmit = Enum.reject(buffers, &is_nil/1)
 
     unless buffers_to_retransmit == [] do
-      Membrane.Logger.debug("Retransmitting buffers: #{inspect(seq_nums_to_retransmit)}")
+      Membrane.Logger.debug(
+        "Retransmitting #{length(buffers_to_retransmit)} buffer(s): #{inspect(Enum.map(buffers_to_retransmit, & &1.metadata.rtp.sequence_number))}"
+      )
     end
 
-    {[buffer: {:output, buffers_to_retransmit}], state}
+    {[buffer: {:output, buffers_to_retransmit}], %{state | store: store}}
   end
 
   @impl true
   def handle_event(pad, event, ctx, state), do: super(pad, event, ctx, state)
 
-  defp maintain_store_size(store) do
-    if BufferStore.size(store) > @max_store_size do
-      {_entry, store} = BufferStore.flush_one(store)
-      store
+  defp sn_to_index(seq_num), do: rem(seq_num, @max_store_size)
+
+  defp maybe_retransmit(seq_num, now, store) do
+    idx = rem(seq_num, @max_store_size)
+    {last_rtx_time, buffer} = Map.get(store, idx, {nil, nil})
+
+    if buffer != nil and buffer.metadata.rtp.sequence_number == seq_num and
+         time_elapsed?(last_rtx_time, now) do
+      store = Map.put(store, idx, {now, buffer})
+      {buffer, store}
     else
-      store
+      {nil, store}
     end
   end
+
+  defp time_elapsed?(nil, _now), do: true
+  defp time_elapsed?(last_rtx_time, now), do: now - last_rtx_time >= @min_rtx_interval
 end
