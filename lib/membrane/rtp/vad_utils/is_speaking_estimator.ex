8a5fffc6bd4c322862e32916f2a526fea8125916
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-03-13 12:33:10 +0100

    Count immediates

--- lib/membrane/rtp/vad_utils/is_speaking_estimator.ex
+++ lib/membrane/rtp/vad_utils/is_speaking_estimator.ex
@@ -5,15 +5,18 @@ defmodule Membrane.RTP.VadUtils.IsSpeakingEstimator do
   by Ilana Volfin and Israel Cohen"
   """
 
-  @n1 13
-  @n2 5
-  @n3 10
+  @n1 2
+  @n2 1
+  @n3 1
   @c1 1
   @c2 1
   @c3 1
 
+  @min_levels_length @n1*@n2*@n3
+
   # @max_level 127
   # @min_level 0
+  @level_threshold 5
 
   @min_activity_score 1.0e-8
 
@@ -35,13 +38,34 @@ defmodule Membrane.RTP.VadUtils.IsSpeakingEstimator do
     if score > @min_activity_score, do: score, else: @min_activity_score
   end
 
+  defp indicator_func(x, threshold) do
+    if x >= threshold, do: 1, else: 0
+  end
+
+  defp count_active(data, threshold) do
+     Enum.reduce(data, 0,
+          fn x, acc -> acc + indicator_func(x, threshold) end)
+  end
+
+  defp compute_immediates(levels) do
+    levels |>
+    Enum.chunk_every(@n1) |>
+    Enum.map(&(count_active(&1, @level_threshold)))
+  end
+
+  def estimate_is_speaking(levels) when length(levels) < @min_levels_length,
+    do: :silence
+
   @spec estimate_is_speaking(list(integer)) :: (:speech | :silence)
   def estimate_is_speaking(levels) do
-    IO.inspect(binomial_coefficient(5, 5))
-    IO.inspect(compute_activity_score(100, 85, 0.78))
-    IO.inspect(levels)
-    IO.inspect("#{@n1}, #{@n2}, #{@n3}, #{@c1}, #{@c2}, #{@c3}")
+    levels |>
+    compute_immediates() |>
+    Enum.map(&(compute_activity_score(@n1, &1, 0.78))) |>
+    IO.inspect()
+
     if :true ,do: :speech, else: :silence
   end
+
+
 
 end
