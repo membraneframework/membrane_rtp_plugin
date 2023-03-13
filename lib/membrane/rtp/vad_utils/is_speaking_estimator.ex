ddff6a5cb55de4c73540760ae83dc76b05c4d589
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-03-13 09:38:51 +0100

    computing activity score function

--- lib/membrane/rtp/vad_utils/is_speaking_estimator.ex
+++ lib/membrane/rtp/vad_utils/is_speaking_estimator.ex
@@ -11,11 +11,11 @@ defmodule Membrane.RTP.VadUtils.IsSpeakingEstimator do
   @c1 1
   @c2 1
   @c3 1
-  # @db_threshold 50
 
   # @max_level 127
   # @min_level 0
 
+  @min_activity_score 1.0e-8
 
   defp binomial_coefficient(n, k) when k < 0 or k > n, do: 0
   defp binomial_coefficient(n, k) when k == 0 or n == k , do: 1
@@ -26,10 +26,19 @@ defmodule Membrane.RTP.VadUtils.IsSpeakingEstimator do
     Enum.reduce(1, fn i, acc -> (div(acc*(n-i+1), i)) end)
   end
 
+  defp compute_activity_score(n_r, vl, lambda) do
+    p = 0.5
+    score = :math.log(binomial_coefficient(n_r, vl)) +
+            vl * :math.log(p) +
+            (n_r - vl) * :math.log(1-p) -
+            :math.log(lambda) + lambda * vl
+    if score > @min_activity_score, do: score, else: @min_activity_score
+  end
+
   @spec estimate_is_speaking(list(integer)) :: (:speech | :silence)
   def estimate_is_speaking(levels) do
     IO.inspect(binomial_coefficient(5, 5))
-    IO.inspect(binomial_coefficient(5, 2))
+    IO.inspect(compute_activity_score(100, 85, 0.78))
     IO.inspect(levels)
     IO.inspect("#{@n1}, #{@n2}, #{@n3}, #{@c1}, #{@c2}, #{@c3}")
     if :true ,do: :speech, else: :silence
