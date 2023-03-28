2f4a567608dc7d46d9fafdab027bcde448ac5b2e
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2023-03-28 09:49:26 +0200

    First version of is_speaking_esimator tests added

--- config/test.exs
+++ config/test.exs
@@ -5,9 +5,9 @@ config :membrane_rtp_plugin,
     n1: 2,
     n2: 2,
     n3: 2,
-    medium_subunit_threshold: 1,
-    long_subunit_threshold: 2,
-    immediate_score_threshold: 0,
-    medium_score_threshold: 0,
-    long_score_threshold: 0
+    medium_subunit_threshold: 2,
+    long_subunit_threshold: 1,
+    immediate_score_threshold: 0.1,
+    medium_score_threshold: 0.1,
+    long_score_threshold: 0.1
   ]
--- test/membrane/rtp/vad_utils/is_speaking_estimator_test.exs
+++ test/membrane/rtp/vad_utils/is_speaking_estimator_test.exs
@@ -3,39 +3,59 @@ defmodule Membrane.RTP.VadUtils.IsSpeakingEstimatorTest do
 
   use ExUnit.Case
 
-  # alias Membrane.RTP.VadUtils.IsSpeakingEstimator
+  alias Membrane.RTP.VadUtils.IsSpeakingEstimator
 
   @algorithm_parameters Application.compile_env(:membrane_rtp_plugin, :vad_estimation_parameters)
+  @expected_levels_length @algorithm_parameters[:n1] * @algorithm_parameters[:n2] *
+                            @algorithm_parameters[:n3]
 
   describe "estimate is speaking" do
     test "returns :silence when levels length less than target length" do
-      # TODO - implement me
+      levels = [100, 100, 100, 100, 100, 100]
+      dummy_threshold = 50
+
+      assert IsSpeakingEstimator.estimate_is_speaking(levels, dummy_threshold) == :silence
     end
 
     test "returns :silence when digital silence" do
-      # TODO - implement me
+      levels = [0, 0, 0, 0, 0, 0, 0, 0]
+      threshold = 50
+
+      assert IsSpeakingEstimator.estimate_is_speaking(levels, threshold) == :silence
     end
 
     test "returns :speech when digital noise" do
-      # TODO - implement me
+      levels = [100, 100, 100, 100, 100, 100, 100, 100]
+      threshold = 50
+
+      assert IsSpeakingEstimator.estimate_is_speaking(levels, threshold) == :speech
     end
 
     test "returns :silence for alternating signal" do
-      # TODO - implement me
+      levels = [100, 0, 100, 0, 100, 0, 100, 0]
+      threshold = 50
+
+      assert IsSpeakingEstimator.estimate_is_speaking(levels, threshold) == :silence
     end
 
     test "returns :speech for alternating pairs" do
-      # TODO - implement me
+      levels = [100, 100, 0, 0, 100, 100, 0, 0]
+      threshold = 50
+
+      assert IsSpeakingEstimator.estimate_is_speaking(levels, threshold) == :speech
     end
 
     test "returns :speech for first half above threshold" do
-      # TODO - implement me
+      levels = [100, 100, 100, 100, 0, 0, 0, 0]
+      threshold = 50
+
+      assert IsSpeakingEstimator.estimate_is_speaking(levels, threshold) == :speech
     end
   end
 
   describe "get target levels length" do
     test "returns expected target levels length" do
-      # TODO - implement me
+      assert IsSpeakingEstimator.get_target_levels_length() == @expected_levels_length
     end
   end
 end
