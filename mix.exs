2cfb9545d5a8eb20824b158df3482dd8fdd7944a
Mateusz Front <mateusz.front@swmansion.com>
2023-11-27 11:25:09 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.24.0"
+  @version "0.24.1"
   @github_url "https://github.com/membraneframework/membrane_rtp_plugin"
 
   def project do
