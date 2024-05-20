edd7b262b2bab57b8e41502457d89cd47f1b3430
Karol Konkol <konkolisz@gmail.com>
2024-05-20 17:53:17 +0200

    Release 0.28.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.27.1"
+  @version "0.28.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_plugin"
 
   def project do
