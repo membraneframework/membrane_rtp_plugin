b92fe0e194d72ad8816f171dd3ee45da8ba59755
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2025-01-06 19:34:07 +0100

    RTP Muxer (#173)
    * Start working on the muxer
    
    * Add working test
    
    * WIP
    
    * Add demuxer_muxer test
    
    * Remove muxer_demuxer test
    
    * Improve demuxer_muxer_test
    
    * Satisfy credo
    
    * Satisfy credo
    
    * Adapt to new resolve return type
    
    * Adjust test
    
    * Bump version
    
    * Remove unused deps
    
    * Remove an unused field from State
    
    * Restore removed deps
    
    * Apply reviewers suggestion
    
    * Add muxer demuxer test
    
    * Apply reviewers suggestion

---
+++ test/membrane/rtp/demuxer_muxer_integration_test.exs
@@ -0,0 +1,127 @@
+defmodule Membrane.RTP.DemuxerMuxerTest do
+  @moduledoc false
+  use ExUnit.Case
+  import Membrane.Testing.Assertions
+  alias Membrane.RTP
+  alias Membrane.Testing
+
+  @rtp_input %{
+    pcap_path: "test/fixtures/rtp/session/demo_rtp.pcap",
+    packets: 862
+  }
+
+  defmodule ReferencePipeline do
+    use Membrane.Pipeline
+
+    @impl true
+    def handle_init(_ctx, opts) do
+      spec =
+        child(:source, %Membrane.Pcap.Source{path: opts.input_path})
+        |> child(:sink, Membrane.Testing.Sink)
+
+      {[spec: spec], %{}}
+    end
+  end
+
+  defmodule SubjectPipeline do
+    use Membrane.Pipeline
+
+    @impl true
+    def handle_init(_ctx, opts) do
+      spec = [
+        child(:source, %Membrane.Pcap.Source{path: opts.input_path})
+        |> child(:rtp_demuxer, Membrane.RTP.Demuxer),
+        child(:rtp_muxer, Membrane.RTP.Muxer)
+        |> child(:sink, Membrane.Testing.Sink)
+      ]
+
+      {[spec: spec], %{}}
+    end
+
+    @impl true
+    def handle_child_notification(
+          {:new_rtp_stream, %{ssrc: ssrc, payload_type: pt}},
+          :rtp_demuxer,
+          _ctx,
+          state
+        ) do
+      %{encoding_name: encoding_name, clock_rate: clock_rate} =
+        Membrane.RTP.PayloadFormat.get_payload_type_mapping(pt)
+
+      spec =
+        get_child(:rtp_demuxer)
+        |> via_out(:output, options: [stream_id: {:ssrc, ssrc}])
+        |> child({:jitter_buffer, ssrc}, %Membrane.RTP.JitterBuffer{clock_rate: clock_rate})
+        |> via_in(:input, options: [encoding: encoding_name])
+        |> get_child(:rtp_muxer)
+
+      {[spec: spec], state}
+    end
+  end
+
+  test "Demuxed and muxed stream is the same as unchanged one" do
+    reference_pipeline =
+      Testing.Pipeline.start_supervised!(
+        module: ReferencePipeline,
+        custom_args: %{input_path: @rtp_input.pcap_path}
+      )
+
+    subject_pipeline =
+      Testing.Pipeline.start_supervised!(
+        module: SubjectPipeline,
+        custom_args: %{input_path: @rtp_input.pcap_path}
+      )
+
+    assert_start_of_stream(reference_pipeline, :sink)
+    assert_start_of_stream(subject_pipeline, :sink)
+
+    reference_normalized_packets = get_normalized_packets(reference_pipeline, @rtp_input.packets)
+    subject_normalized_packets = get_normalized_packets(subject_pipeline, @rtp_input.packets)
+
+    assert reference_normalized_packets == subject_normalized_packets
+
+    assert_end_of_stream(reference_pipeline, :sink)
+    assert_end_of_stream(subject_pipeline, :sink)
+    Testing.Pipeline.terminate(reference_pipeline)
+    Testing.Pipeline.terminate(subject_pipeline)
+  end
+
+  @spec get_normalized_packets(pid(), non_neg_integer()) ::
+          %{RTP.encoding_name() => [ExRTP.Packet.t()]}
+  defp get_normalized_packets(pipeline, buffers_amount) do
+    Enum.map(1..buffers_amount, fn _i ->
+      assert_sink_buffer(pipeline, :sink, %Membrane.Buffer{
+        payload: payload
+      })
+
+      {:ok, packet} = ExRTP.Packet.decode(payload)
+
+      packet
+    end)
+    |> Enum.group_by(& &1.payload_type)
+    |> Map.new(fn {payload_type, payload_type_buffers} ->
+      %{encoding_name: encoding_name} = RTP.PayloadFormat.get_payload_type_mapping(payload_type)
+
+      sorted_packets =
+        payload_type_buffers
+        |> Enum.sort(&(&1.sequence_number < &2.sequence_number))
+
+      %{ssrc: ssrc, sequence_number: first_sequence_number, timestamp: first_timestamp} =
+        List.first(sorted_packets)
+
+      normalized_packets =
+        sorted_packets
+        |> Enum.map(fn packet ->
+          %{
+            packet
+            | ssrc: packet.ssrc - ssrc,
+              sequence_number: packet.sequence_number - first_sequence_number,
+              # round to ignore insignificant differences in timestamps
+              timestamp: round((packet.timestamp - first_timestamp) / 10)
+          }
+        end)
+
+      {encoding_name, normalized_packets}
+    end)
+  end
+end
---
+++ test/membrane/rtp/muxer_demuxer_integration_test.exs
@@ -0,0 +1,54 @@
+defmodule Membrane.RTP.MuxerDemuxerTest do
+  @moduledoc false
+  use ExUnit.Case
+  import Membrane.Testing.Assertions
+  alias Membrane.RTP
+  alias Membrane.Testing
+
+  @input_path "test/fixtures/rtp/h264/bun.h264"
+
+  defmodule MuxerDemuxerPipeline do
+    use Membrane.Pipeline
+
+    @impl true
+    def handle_init(_ctx, opts) do
+      %{clock_rate: clock_rate} = RTP.PayloadFormat.resolve(encoding_name: :H264)
+
+      spec = [
+        child(:source, %Membrane.File.Source{location: opts.input_path})
+        |> child(:h264_parser, %Membrane.H264.Parser{
+          output_alignment: :nalu,
+          generate_best_effort_timestamps: %{framerate: {30, 1}}
+        })
+        |> child(:rtp_h264_payloader, Membrane.RTP.H264.Payloader)
+        |> via_in(:input, options: [encoding: :H264])
+        |> child(:rtp_muxer, Membrane.RTP.Muxer)
+        |> child(:rtp_demuxer, Membrane.RTP.Demuxer)
+        |> via_out(:output, options: [stream_id: {:encoding_name, :H264}])
+        |> child(:jitter_buffer, %Membrane.RTP.JitterBuffer{clock_rate: clock_rate})
+        |> child(:rtp_h264_depayloader, Membrane.RTP.H264.Depayloader)
+        |> child(:sink, %Membrane.File.Sink{location: opts.output_path})
+      ]
+
+      {[spec: spec], %{}}
+    end
+  end
+
+  @tag :tmp_dir
+  test "Muxed and demuxed stream is the same as unchanged one", %{tmp_dir: tmp_dir} do
+    output_path = Path.join(tmp_dir, "output.h264")
+
+    pipeline =
+      Testing.Pipeline.start_supervised!(
+        module: MuxerDemuxerPipeline,
+        custom_args: %{input_path: @input_path, output_path: output_path}
+      )
+
+    assert_start_of_stream(pipeline, :sink)
+    assert_end_of_stream(pipeline, :sink)
+
+    assert File.read!(@input_path) == File.read!(output_path)
+
+    Testing.Pipeline.terminate(pipeline)
+  end
+end
