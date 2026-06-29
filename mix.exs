42e3e370b21ef0b460e434070b3e71f54f6da23d
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:27:55 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -63,7 +63,7 @@ defmodule Membrane.RTP.Plugin.MixProject do
       {:membrane_realtimer_plugin, "~> 0.10.1", only: :test},
 
       # Dev
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
